ba3548f5abd6bdf736da36c36f64f72e25e36c73
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-29 16:27:34 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 744 ms, Memory: 89728 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -1,23 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())  # 이번 학기 시간
-score = 0  # 최종 결과 (이번학기에 받을 수 있는 총점
-stack = []
+n = int(input())
+stack = []  # 현재 하고 있는 과제만 관리
+ans = 0     # 최종 점수
 
-# 이번 학기 시간동안 반복
-for _ in range(N):
-    info = list(map(int, input().split()))  # 
+for _ in range(n):
+    query = input().split()
+    
+    if query[0] == "1":
+        score, time = int(query[1]), int(query[2])
+        if time == 1:
+            ans += score
+        else:
+            stack.append([score, time-1])  # 새로운 과제를 시작
+    else:
+        if stack:
+            stack[-1][1] -= 1
+            if not stack[-1][1]:
+                ans += stack.pop()[0]
 
-    # 과제 생기면 스택에 저장
-    if info[0] == 1:
-        a, t = info[1], info[2]
-        stack.append([a, t])
-
-    # 과제 하기
-    if stack: # 스택이 비었을 수도 있으므로 체크
-        stack[-1][1] -= 1 # 하던 과제를 해야지...(에휴)
-        if stack[-1][1] == 0: # 과제 다했다면 점수 추가
-            score += stack.pop()[0]
-
-print(score)
+print(ans)
