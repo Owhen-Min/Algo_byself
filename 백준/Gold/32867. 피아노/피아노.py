2334a2a92e0b63c3630ad194190c23a73586368c
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-05-07 17:38:24 +0900

    [Gold V] Title: 피아노, Time: 124 ms, Memory: 126324 KB -BaekjoonHub

---
+++ 백준/Gold/32867. 피아노/피아노.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+ans = -1
+min_note = max_note = -1000
+
+for note in map(int, input().split()):
+    if note > max_note:
+        max_note = note
+    elif note < min_note:
+        min_note = note
+    else: continue
+
+    if max_note - min_note >= k:
+        ans += 1
+        min_note = max_note = note
+
+print(ans)
