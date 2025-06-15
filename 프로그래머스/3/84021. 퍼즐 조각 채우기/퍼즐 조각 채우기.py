9700841ea7abb7065575d09057f51044375e840d
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-06-15 23:25:39 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 10.20 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
@@ -1,98 +1,59 @@
-from collections import Counter
-from dataclasses import dataclass
-from itertools import product
-
-
-@dataclass(frozen=True)
-class Pos:
-    x: int
-    y: int
-
-    def neighbors(self):
-        return [
-            Pos(self.x, self.y - 1),
-            Pos(self.x + 1, self.y),
-            Pos(self.x, self.y + 1),
-            Pos(self.x - 1, self.y),
-        ]
-
-
-def make_tile_from_positions(positions):
-    """Smallest possible representation with rotation"""
-
-    def rotate90(tile):
-        return tuple(
-            tuple(tile[i][j] for i in range(len(tile)))
-            for j in reversed(range(len(tile[0])))
-        )
-
-    positions = set(positions)
-
-    xs = [pos.x for pos in positions]
-    min_x = min(xs)
-    max_x = max(xs)
-
-    ys = [pos.y for pos in positions]
-    min_y = min(ys)
-    max_y = max(ys)
-
-    tile_representations = [
-        tuple(
-            tuple(Pos(i, j) in positions for j in range(min_y, max_y + 1))
-            for i in range(min_x, max_x + 1)
-        )
-    ]
-
-    for __ in range(3):
-        tile_representations.append(rotate90(tile_representations[-1]))
-
-    return min(tile_representations)
-
-
-def get_tile_size(tile):
-    return sum(sum(row) for row in tile)
-
-
-def parse_tiles(board, tile_value=1):
-    n = len(board)
-
-    # Add sentinel boundaries
-    sentinel = 1 - tile_value
-
-    board = [
-        [sentinel] * (n + 2),
-        *([sentinel] + row + [sentinel] for row in board),
-        [sentinel] * (n + 2),
-    ]
-
-    # Detect tiles
-    tile_positions = []
-    for i, j in product(range(1, n + 1), range(1, n + 1)):
-        if board[i][j] == tile_value:
-            stack = [Pos(i, j)]
-            squares = []
-            while stack:
-                curr = stack.pop()
-                board[curr.x][curr.y] = sentinel
-                squares.append(curr)
-                for neighbor in curr.neighbors():
-                    if board[neighbor.x][neighbor.y] == tile_value:
-                        stack.append(neighbor)
-            tile_positions.append(squares)
-
-    # Make tiles
-    tiles = [make_tile_from_positions(p) for p in tile_positions]
-
-    return tiles
-
-
 def solution(game_board, table):
-    tiles = parse_tiles(table, 1)
-    empty_spaces = parse_tiles(game_board, 0)
+    from collections import defaultdict
+    blanks = defaultdict(int)
+    n, m = len(game_board), len(game_board[0])
 
-    tile_counter = Counter(tiles)
-    empty_space_counter = Counter(empty_spaces)
+    def dfs(board, start_y, start_x, is_table):
+        points = [(0, 0)]
+        totals = 0
+        stack = [(start_y, start_x)]
+        if is_table:
+            board[start_y][start_x] = 0
+            totals += 1
+        else: board[start_y][start_x] = 1
 
-    used_tiles = tile_counter & empty_space_counter
 
-    return sum(get_tile_size(tile) * occ for tile, occ in used_tiles.items())
+        while stack:
+            y, x = stack.pop()
+            for dy, dx in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                ny, nx = y+dy, x+dx
+                if 0<=ny < n and 0<=nx<m and board[ny][nx]==is_table:
+                    points.append((ny-start_y, nx-start_x))
+                    stack.append((ny, nx))
+                    if is_table:
+                        board[ny][nx] = 0
+                        totals += 1
+                    else:
+                        board[ny][nx] = 1
+
+        if is_table:
+            return [normalize(points), normalize([(-x, y) for y, x in points]), normalize([(-y, -x) for y, x in points]), normalize([(x, -y) for y, x in points])], totals
+
+        return normalize(points), 0
+
+    # 모든 좌표를 0~6 안으로 들어올 수 있게 보정해서 tuple로 반환. set로 관리할 수 있음.
+    def normalize(points):
+        # 모든 좌표를 0~6 안으로 들어올 수 있게 보정
+        pivot_y = min(point[0] for point in points)
+        pivot_x = min(point[1] for point in points)
+        return tuple(sorted((point[0]-pivot_y, point[1]-pivot_x) for point in points))
+
+    for i in range(n):
+        for j in range(m):
+            if not game_board[i][j]:
+                blank = dfs(game_board, i, j,False)[0]
+                blanks[blank] += 1
+    del game_board, blank
+
+    answer = 0
+    for i in range(n):
+        for j in range(m):
+            if table[i][j]:
+                pieces, total = dfs(table, i, j, True)
+                for piece in pieces:
+                    if blanks[piece]:
+                        blanks[piece] -= 1
+                        answer += total
+                        break
+
+    return answer
