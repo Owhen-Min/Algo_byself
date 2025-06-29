325a7c4112c530697712b6fdd9e18450b3a3d403
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-06-30 01:03:26 +0900

    [Gold IV] Title: 햄버거최대 몇개드실수있나요?, Time: 276 ms, Memory: 57248 KB -BaekjoonHub

--- 백준/Gold/27923. 햄버거최대 몇개드실수있나요？/햄버거최대 몇개드실수있나요？.py
+++ 백준/Gold/27923. 햄버거최대 몇개드실수있나요？/햄버거최대 몇개드실수있나요？.py
@@ -2,9 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n, k, l = map(int, input().split())
-h_list = sorted(list(map(int, input().split())))
+h = sorted(list(map(int, input().split())))
 
-effect = [0]*n
+effect = [0]*(n)
 for i in map(int, input().split()):
     effect[i-1]+=1
     if i+l <= n:
@@ -14,9 +14,5 @@ for i in range(1,n):
     effect[i] += effect[i-1]
 effect.sort()
 
-
-result = 0
-for j in range(n):
-    if effect[j]<64: #포인트임
-        result += h_list[j]>>effect[j]
+result = sum(h[j] >> effect[j] for j in range(n) if effect[j] <64)
 print(result)
