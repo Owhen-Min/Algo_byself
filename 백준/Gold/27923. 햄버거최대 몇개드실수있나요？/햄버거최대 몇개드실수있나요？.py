de51d8b1a96284fc71606f2443071fe3fa26f556
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-06-30 00:41:35 +0900

    [Gold IV] Title: 햄버거최대 몇개드실수있나요?, Time: 220 ms, Memory: 145660 KB -BaekjoonHub

--- 백준/Gold/27923. 햄버거최대 몇개드실수있나요？/햄버거최대 몇개드실수있나요？.py
+++ 백준/Gold/27923. 햄버거최대 몇개드실수있나요？/햄버거최대 몇개드실수있나요？.py
@@ -8,20 +8,14 @@ diff = [0]*n
 m = sorted(map(int, input().split()))
 
 for num in map(int,input().split()):
-    try:
-        diff[num-1] += 1
+    diff[num-1] += 1
+    
+    if num+l <=n:
         diff[num+l-1] -= 1
-    except IndexError:
-        pass
-
+    
 for i in range(n-1):
     diff[i+1] += diff[i]
 
 diff.sort()
 
-ans = 0
-
-for i in range(n):
-    ans += m[i]//(2**diff[i])
-
-print(ans)
+print(sum(a>>coke for a, coke in zip(m, diff)))
