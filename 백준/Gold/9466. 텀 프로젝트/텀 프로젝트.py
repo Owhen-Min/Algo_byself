33184e754b76d4bb223c026978b89471f752e779
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-07-04 16:50:18 +0900

    [Gold III] Title: 텀 프로젝트, Time: 812 ms, Memory: 231680 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,34 +1,28 @@
-for _ in range(int(input())):
-    n = int(input())
-    nxt = [x-1 for x in map(int, input().split())]
-    
-    # -1=미방문, 0 이상=방문 시점
-    visited = [-1] * n
-    on_stack = [False] * n
-    cycle_nodes = 0
-    
-    for start in range(n):
-        if visited[start] != -1:
-            continue
-            
-        curr = start
-        time = 0
-        
-        # DFS 탐색
-        while visited[curr] == -1:
-            visited[curr] = time
-            on_stack[curr] = True
-            time += 1
-            curr = nxt[curr]
-            
-        # 사이클 발견
-        if on_stack[curr]:
-            cycle_nodes += time - visited[curr]
-            
-        # 스택에서 노드 제거
-        curr = start
-        while on_stack[curr]:
-            on_stack[curr] = False
-            curr = nxt[curr]
-    
-    print(n - cycle_nodes)
+def main():
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [a - 1 for a in (map(int, input().split()))]
+
+        # 각 노드의 진입 차수(indegree) 계산
+        counts = [0] * n
+        for a in arr:
+            counts[a] += 1
+
+        # 진입 차수가 0인 노드들을 찾음 (시작점들)
+        ends = [i for i, c in enumerate(counts) if c == 0]
+        i = 0
+
+        # 위상 정렬 유사 알고리즘
+        while i < len(ends):
+            x = arr[ends[i]]  # 현재 노드가 가리키는 노드
+            i += 1
+            counts[x] -= 1    # 진입 차수 감소
+            if counts[x] == 0:  # 새로운 진입 차수 0 노드 발견
+                ends.append(x)
+
+        # 진입 차수가 0인 노드 수 = 사이클에 포함되지 않은 노드 수
+        ans = counts.count(0)
+        print(ans)
+
+if __name__ == "__main__":
+    main()
