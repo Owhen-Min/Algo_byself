fcbcfc0dd025b417c832e112fc09af8afd9dd1e0
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-07-20 15:44:01 +0900

    [Gold III] Title: ACM Craft, Time: 412 ms, Memory: 130760 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -3,44 +3,46 @@ from heapq import heappush, heappop, heapify
 import sys
 input = sys.stdin.readline
 
-
 for _ in range(int(input())):
-    n, k = map(int, input().split())
+    n, k = map(int,input().split())
     # 위상정렬을 위한 빈 리스트 buildings
-    buildings = [0] * n
+    buildings = [0]*n
 
     # 건물을 짓는 데 드는 시간을 담을 변수 times
-    times = tuple(map(int, input().split()))
+    times = tuple(map(int,input().split()))
 
     # 건물이 완성되고 나서 제약사항을 확인할 dict orders
     orders = defaultdict(list)
 
     for __ in range(k):
-        x, y = map(int, input().split())
-        orders[x - 1].append(y - 1)
-        buildings[y - 1] += 1
+        x, y = map(int,input().split())
+        orders[x-1].append(y-1)
+        buildings[y-1] += 1
 
-    target = int(input()) - 1
+    target = int(input()) -1
 
-    # 현재 시간
-    current_time = 0
+    ans = 0
 
-    # 현재 지을 수 있는 건물들 (시간, 건물 인덱스)
-    inbuild = [(times[index], index) for index, enter in enumerate(buildings) if enter == 0]
+    # 현재 지어지고 있는 건물의 리스트를 담을 inbuild
+    inbuild = [[times[index], index] for index, enter in enumerate(buildings) if enter == 0]
+
+    # 가장 시간이 적게 드는 애 기준으로 나머지 애들의 시간을 줄여줄거니까 heap 구조로 만듦.
     heapify(inbuild)
 
-    while inbuild:
+    while True:
         build_time, curr = heappop(inbuild)
 
-        current_time = build_time
-
+        ans += build_time
         if curr == target:
             break
 
-        # 다음에 지을 수 있는 건물들 추가
+        if build_time:
+            inbuild = [[x[0] - build_time, x[1]] for x in inbuild]
+
         for nxt in orders[curr]:
             buildings[nxt] -= 1
             if buildings[nxt] == 0:
-                heappush(inbuild, (current_time + times[nxt], nxt))
+                heappush(inbuild, [times[nxt], nxt])
 
-    print(current_time)
+
+    print(ans)
