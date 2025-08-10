695f203c27b6fbd380b68758b985cf8a924c8f2a
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-08-11 03:24:55 +0900

    [Gold III] Title: 줄 세우기, Time: 176 ms, Memory: 120680 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,39 +1,30 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
 
 
-n,m = map(int,input().split())
-taller = defaultdict(list)
-shorter = defaultdict(list)
-ls = [0]*(n+1)
+n, m = map(int,input().split())
+# 인접 그래프 생성
+graph = defaultdict(list)
+# 위상 정렬을 위한 진입 차수를 기록할 indegree
+indegree = [0]*(n+1)
 
 for _ in range(m):
     s, e = map(int,input().split())
-    taller[s].append(e)
-    shorter[e].append(s)
-    ls[e] += 1
+    graph[s].append(e)
+    indegree[e] += 1
 
+# 진입차수가 0인 (얘보다 더 작을 애가 없는) 애들은 다 때려 넣는다
+q = deque(i for i in range(1,n+1) if indegree[i] == 0)
 ans = []
-temp = []
-def lining(node):
-    while True:
-        if ls[node] == -1:
-            return
-        elif ls[node] == 0:
-            temp.append(node)
-            ls[node] = -1
-            for nxt in taller[node]:
-                ls[nxt] -= 1
-            return
-        else:
-            for nxt in shorter[node]:
-                lining(nxt)
 
-for i in range(1,n+1):
-    temp = []
-    lining(i)
-    ans += temp
+while q:
+    curr = q.popleft()
+    ans.append(curr)
 
-print(' '.join(map(str, ans)))
+    for nxt in graph[curr]:
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            q.append(nxt)
+
+print(*ans)
