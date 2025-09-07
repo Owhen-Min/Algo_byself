32c4bdf41796c3dfe140035989ba17d83f2182a9
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-09-08 02:24:51 +0900

    [Gold II] Title: 빵집, Time: 756 ms, Memory: 310016 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+r, c = map(int,input().split())
+board = list(list(input()) for _ in range(r))
+# visited = [[0]*c for _ in range(r)]
+
+# DFS 기준 위 중간 아래 순으로 탐색하도록 설정, 최적값 보장
+deltas = ((1,1),(0,1),(-1,1))
+ans = 0
+
+for i in range(r):
+    stack = [(i,0)]
+    while stack:
+        y, x = stack.pop()
+        board[y][x] = 'x'
+        if x == c-2:
+            ans += 1
+            break
+        for dy, dx in deltas:
+            ny, nx = y+dy, x+dx
+            if 0<=ny<r and board[ny][nx] == '.': stack.append((ny, nx))
+
+print(ans)
