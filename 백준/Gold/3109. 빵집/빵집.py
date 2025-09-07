01c33e6c92bed6ea9757650317184eb5781ec82a
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-09-08 02:26:09 +0900

    [Gold II] Title: 빵집, Time: 404 ms, Memory: 156268 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 r, c = map(int,input().split())
-board = list(list(input()) for _ in range(r))
-# visited = [[0]*c for _ in range(r)]
+board = list(input() for _ in range(r))
+visited = [[0]*c for _ in range(r)]
 
 # DFS 기준 위 중간 아래 순으로 탐색하도록 설정, 최적값 보장
 deltas = ((1,1),(0,1),(-1,1))
@@ -13,12 +13,13 @@ for i in range(r):
     stack = [(i,0)]
     while stack:
         y, x = stack.pop()
-        board[y][x] = 'x'
+        visited[y][x] = 1
         if x == c-2:
             ans += 1
             break
         for dy, dx in deltas:
             ny, nx = y+dy, x+dx
-            if 0<=ny<r and board[ny][nx] == '.': stack.append((ny, nx))
+            if 0<=ny<r and board[ny][nx] == '.' and not visited[ny][nx]:
+                stack.append((ny, nx))
 
 print(ans)
