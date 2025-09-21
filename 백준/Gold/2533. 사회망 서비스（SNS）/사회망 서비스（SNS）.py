7ec1bdcc575493fee402268ca714a51d71cd7f36
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-09-22 03:04:47 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1876 ms, Memory: 267824 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,39 +1,36 @@
 import sys
-from array import array
+# from array import array
 input = sys.stdin.readline
 
 n = int(input())
-graph = [array("i") for _ in range(n+1)]
+
+# graph = 인접그래프 / entries = 진입차수
+graph = [[] for _ in range(n+1)]
+entries = [0]*(n+1)
 
 for _ in range(n-1):
-    a, b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    x, y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    entries[x]+= 1
+    entries[y]+= 1
 
-# dp[i][0] = i번째 노드가 얼리어답터가 아닐 때 조건을 충족하는 최소한의 얼리어답터수
-# dp[i][1] = i번째 노드가 얼리어답터일 때 조건을 충족하는 최소한의 얼리어답터수
-dp = [[0,0] for _ in range(n+1)]
+# 진입차수가 1인 리프노드들부터 탐색하면서 올라간다.
+stack = [i for i in range(1, n+1) if entries[i] == 1]
 
-# stack.pop() = 현재 위치, 부모노드, 자식노드의 계산이 끝났는지 여부
-stack = [(1, -1, False)]
+# early = i번째 사람이 얼리어뎁터인지 기록
+early = [0]*(n+1)
 
 while stack:
-    curr, parent, flag = stack.pop()
-    # 만약 자식 노드들을 다 방문하지 않아서 최소값을 모르는 경우, 다음에 오라고 놓아주기
-    if not flag:
-        stack.append((curr, parent, True))
-        for child in graph[curr]:
-            if child != parent:
-                stack.append((child, curr, False))
-    # 자식들의 노드 방문이 끝난 후 계산
-    else:
-        dp[curr][0] = 0
-        dp[curr][1] = 1
-        for child in graph[curr]:
-            if child != parent:
-                # 내가 얼리어답터가 아닌 경우 자식은 꼭 얼리어답터여야 함
-                dp[curr][0] += dp[child][1]
-                # 내가 얼리어답터일 경우 자식은 더 작은 애로 가져오면 됨
-                dp[curr][1] += min(dp[child][0], dp[child][1])
+    curr = stack.pop()
+    is_early = early[curr]
+    for nxt in graph[curr]:
+        entries[nxt] -= 1
+        # 리프 노드를 잘라나가면서 새로운 리프노드가 발견되면 stack에 추가
+        if entries[nxt] == 1:
+            stack.append(nxt)
+        # 해당 리프 노드가 얼리어뎁터가 아니면 부모노드는 얼리어뎁터여야 함.
+        if not is_early:
+            early[nxt] = 1
 
-print(min(dp[1]))
+print(sum(early))
