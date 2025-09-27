d44cb542536c9df27d1be34418e1418a2a60955f
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-09-27 17:46:05 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 116 ms, Memory: 111004 KB -BaekjoonHub

---
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
@@ -0,0 +1,29 @@
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+
+left = 0
+right = max(arr)-min(arr)
+
+def can_divide(target, max_tries):
+    cnt = 1
+    min_sec = max_sec = arr[0]
+
+    for i in range(n):
+        min_sec = min(arr[i], min_sec)
+        max_sec = max(arr[i], max_sec)
+        if max_sec - min_sec > target:
+            min_sec = max_sec = arr[i]
+            cnt += 1
+            if cnt > max_tries:
+                return False
+
+    return True
+
+while left <= right:
+    mid = (left+right)//2
+    if can_divide(mid, m):
+        right = mid-1
+    else:
+        left = mid+1
+
+print(left)
