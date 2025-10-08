1e095c2d21e07035ffcfc506dcd42474f2a29ef8
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-10-08 15:12:37 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,30 +1,20 @@
-from collections import defaultdict
+n, m = map(int,input().split())
+a, *knowers = map(int,input().split())
+knowers = set(knowers)
 
-n, m = map(int, input().split())
-known, *watchers = map(int, input().split())
-watchers = set(watchers)
-visited = [0] * (n+1)
-party_dict = dict()
-person_party_dict = defaultdict(list)
-only_truth_party = set()
+parties = [list(map(int,input().split()))[1:] for _ in range(m)]
 
-for i in range(m):
-    num, *participants = map(int, input().split())
-    party_dict[i] = participants
-    for participant in participants:
-        person_party_dict[participant].append(i)
+prev = 0
+while len(knowers) != prev:
+    prev = len(knowers)
+    for party in parties:
+        if any(part in knowers for part in party):
+            knowers.update(party)
 
-while True:
-    for watcher in watchers:
-        if visited[watcher]:
-            continue
-        else:
-            visited[watcher] = 1
-            only_truth_party.update(person_party_dict[watcher])
-            for party in person_party_dict[watcher]:
-                watchers.update(party_dict[party])
-            break
-    else:
-        break
+ans = 0
 
-print(m-len(only_truth_party))
+for party in parties:
+    if all(part not in knowers for part in party):
+        ans += 1
+
+print(ans)
