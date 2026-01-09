8817dc436677010af7200923ed4d7296dfc8caa9
Owhen-Min <skyblue847@hanyang.ac.kr>
2026-01-10 04:17:21 +0900

    [Gold III] Title: 낚시 (Easy), Time: 244 ms, Memory: 50676 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/34954. 낚시 （Easy）/낚시 （Easy）.py
+++ 백준/Gold/34954. 낚시 （Easy）/낚시 （Easy）.py
@@ -1,27 +1,19 @@
 import heapq
-
 n, m = map(int,input().split())
-baits = sorted(map(int,input().split()))
-fish = sorted(f for f in map(int,input().split()) if f > baits[0])
-m = len(fish)
+baits = list(map(lambda x: -x, map(int,input().split())))
+fish = list(map(lambda x: -x, map(int,input().split())))
+heapq.heapify(baits)
+heapq.heapify(fish)
 
-i = j = 0
 ans = 0
-caught = []
 
-while j<m:
-    # 떡밥으로 물고기를 잡을 수 있을 때 > 무조건 잡는디!
-    if i<n and baits[i] < fish[j]:
-        heapq.heappush(caught, fish[j])
-        ans += fish[j]
-        i += 1
-        j += 1
-    # 떡밥으로 물고기를 잡을 수 없을 때
-    else:
-        # 기존에 물고기 잡아둔게 있다면 물고기로 떡밥을 만들어서 새로운 물고기를 잡는다
-        if caught:
-            ans += fish[j] - heapq.heappop(caught)
-            heapq.heappush(caught, fish[j])
-        j += 1
+while baits and fish:
+    curr_fish = heapq.heappop(fish)
+
+    while baits:
+        curr_bait = heapq.heappop(baits)
+        if curr_bait > curr_fish:
+            ans -= curr_fish
+            break
 
 print(ans)
