e7676bb5be153dd31a2f40862412e918d2a1ce7c
Owhen-Min <skyblue847@hanyang.ac.kr>
2026-01-10 03:57:53 +0900

    [Gold III] Title: 낚시 (Easy), Time: 180 ms, Memory: 137632 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Gold/34954. 낚시 （Easy）/낚시 （Easy）.py
@@ -0,0 +1,26 @@
+import heapq
+
+n, m = map(int,input().split())
+baits = sorted(map(int,input().split()))
+fish = sorted(f for f in map(int,input().split()) if f >= baits[0])
+
+i = j = 0
+ans = 0
+caught = []
+
+while j<m:
+    # 떡밥으로 물고기를 잡을 수 있을 때 > 무조건 잡는디!
+    if i<n and baits[i] < fish[j]:
+        caught.append(fish[j])
+        ans += fish[j]
+        i += 1
+        j += 1
+    # 떡밥으로 물고기를 잡을 수 없을 때
+    else:
+        # 기존에 물고기 잡아둔게 있다면 물고기로 떡밥을 만들어서 새로운 물고기를 잡는다
+        if caught:
+            ans += fish[j] - heapq.heappop(caught)
+            heapq.heappush(caught, fish[j])
+        j += 1
+
+print(ans)
