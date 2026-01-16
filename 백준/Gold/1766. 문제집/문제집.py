b14d1093fe293e3eeb081316b3679d9916095dfb
Owhen-Min <skyblue847@hanyang.ac.kr>
2026-01-17 03:13:43 +0900

    [Gold II] Title: 문제집, Time: 192 ms, Memory: 117628 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,29 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+# order[i] = i번 문제를 풀고난 뒤 풀 수 있는 후행 문제들 리스트
+order = {i: [] for i in range(1,n+1)}
+# procedure[i] = i번 문제를 풀기 전 풀어야 하는 선행문제 개수
+procedure = [0]*(n+1)
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    order[a].append(b)
+    procedure[b] += 1
+
+ans = []
+q = [i for i in range(1,n+1) if procedure[i] < 1]
+
+while q:
+    curr = heapq.heappop(q)
+    ans.append(curr)
+    # 문제를 풀었으니 후행문제들을 확인하고 큐에 넣는다.
+    for nxt in order[curr]:
+        procedure[nxt] -= 1
+        if procedure[nxt] == 0:
+            heapq.heappush(q, nxt)
+
+print(*ans)
